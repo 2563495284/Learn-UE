125e6533bd5f3b69baad513718337bddecef732f
yijin <359222976@qq.com>
2025-10-25 17:41:01 +0800

    *

--- MyProject/Source/MyProject/MyProject.Build.cs
+++ MyProject/Source/MyProject/MyProject.Build.cs
@@ -4,48 +4,51 @@ using UnrealBuildTool;
 
 public class MyProject : ModuleRules
 {
-	public MyProject(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public MyProject(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"Core",
-			"CoreUObject",
-			"Engine",
-			"InputCore",
-			"EnhancedInput",
-			"AIModule",
-			"StateTreeModule",
-			"GameplayStateTreeModule",
-			"UMG",
-			"Slate"
-		});
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "AIModule",
+            "StateTreeModule",
+            "GameplayStateTreeModule",
+            "UMG",
+            "Slate",
+            "GameplayAbilities",
+            "GameplayTags",
+            "GameplayTasks"
+        });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { });
+        PrivateDependencyModuleNames.AddRange(new string[] { });
 
-		PublicIncludePaths.AddRange(new string[] {
-			"MyProject",
-			"MyProject/Variant_Platforming",
-			"MyProject/Variant_Platforming/Animation",
-			"MyProject/Variant_Combat",
-			"MyProject/Variant_Combat/AI",
-			"MyProject/Variant_Combat/Animation",
-			"MyProject/Variant_Combat/Gameplay",
-			"MyProject/Variant_Combat/Interfaces",
-			"MyProject/Variant_Combat/UI",
-			"MyProject/Variant_SideScrolling",
-			"MyProject/Variant_SideScrolling/AI",
-			"MyProject/Variant_SideScrolling/Gameplay",
-			"MyProject/Variant_SideScrolling/Interfaces",
-			"MyProject/Variant_SideScrolling/UI"
-		});
+        PublicIncludePaths.AddRange(new string[] {
+            "MyProject",
+            "MyProject/Variant_Platforming",
+            "MyProject/Variant_Platforming/Animation",
+            "MyProject/Variant_Combat",
+            "MyProject/Variant_Combat/AI",
+            "MyProject/Variant_Combat/Animation",
+            "MyProject/Variant_Combat/Gameplay",
+            "MyProject/Variant_Combat/Interfaces",
+            "MyProject/Variant_Combat/UI",
+            "MyProject/Variant_SideScrolling",
+            "MyProject/Variant_SideScrolling/AI",
+            "MyProject/Variant_SideScrolling/Gameplay",
+            "MyProject/Variant_SideScrolling/Interfaces",
+            "MyProject/Variant_SideScrolling/UI"
+        });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
---
+++ MyProject/Source/MyProject/Public/AbilitySystem/Abilities/PlayerGameplayAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/MyGameplayAbility.h"
+#include "PlayerGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UPlayerGameplayAbility : public UMyGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
